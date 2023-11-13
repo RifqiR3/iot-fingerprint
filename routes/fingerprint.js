8e817ef66c4f6bcf05f08798fc9b070b2e2f3b6a
Rifqi <rifkyirwan68@gmail.com>
2023-11-13 21:21:22 +0800

    update validasi

--- routes/fingerprint.js
+++ routes/fingerprint.js
@@ -9,8 +9,19 @@ router.get('/', function(req, res, next) {
     })
 });
 
+router.get('/validasifinger', function(req, res, next) {
+    res.render("fingervalidasi", {
+        title: 'Validasi',
+        nama: 'Menunggu...',
+        kelas: 'Menunggu...',
+        nim: 'Menunggu...',
+        persentase: 'Menunggu...',
+    })
+});
+
 router.post('/addfinger', function(req, res, next) {
     // console.log(req.body);
+    console.log(req.body);
     var nama = req.body.nama;
     var kelas = req.body.kelas;
     var nim = req.body.nim;
@@ -31,5 +42,104 @@ router.post('/addfinger', function(req, res, next) {
         
     })
 });
+
+router.post('/validasi', function(req, res, next) {
+    let persentaseAwal = 0;
+    let persentaseAkhir = 0;
+    let finger = req.body.fingerValidasi;
+    let query = `SELECT * FROM datamahasiswa`;
+
+    database.query(query, function(err, data) {
+        if (err) {
+            throw err;
+        } else {
+            let hasilData = [{
+                nama: "Tes",
+                kelas: "Tes",
+                nim: "Tes",
+            }];
+
+            for (let index = 0; index <= data.length; index++) {
+                if (data[index] && data[index].fingerPrint) {
+                    persentaseAwal = calculateBinarySimilarity(finger, data[index].fingerPrint);
+                    if (persentaseAkhir <= persentaseAwal) {
+                        hasilData[0].nama = data[index].nama;
+                        hasilData[0].kelas = data[index].kelas;
+                        hasilData[0].nim = data[index].NIM;
+                        persentaseAkhir = persentaseAwal;
+                    }
+                    console.log(data[index].nama);
+                    console.log(data[index].fingerPrint);
+                    console.log(`Persentase: ${persentaseAwal}`);
+
+                    if (index === data.length - 1) {
+                        res.render("fingervalidasi", { 
+                            title: 'Validasi',
+                            nama: hasilData[0].nama,
+                            kelas: hasilData[0].kelas,
+                            nim: hasilData[0].nim,
+                            persentase: persentaseAkhir,
+                        });
+                        break;
+                    }
+                } else {
+                    continue;
+                }
+            }
+        }
+    });
+});
+
+function calculateSimilarity(str1, str2) {
+    const len1 = str1.length + 1;
+    const len2 = str2.length + 1;
+
+    const matrix = Array(len1).fill(null).map(() => Array(len2).fill(0));
+
+    for (let i = 0; i < len1; i++) {
+        for (let j = 0; j < len2; j++) {
+            if (i === 0) {
+                matrix[i][j] = j;
+            } else if (j === 0) {
+                matrix[i][j] = i;
+            } else {
+                const cost = str1[i - 1] === str2[j - 1] ? 0 : 1;
+                matrix[i][j] = Math.min(
+                    matrix[i - 1][j] + 1,
+                    matrix[i][j - 1] + 1,
+                    matrix[i - 1][j - 1] + cost
+                );
+            }
+        }
+    }
+
+    const distance = matrix[len1 - 1][len2 - 1];
+    const maxLength = Math.max(str1.length, str2.length);
+    const similarity = ((maxLength - distance) / maxLength) * 100;
+
+    return similarity.toFixed(2);
+}
+
+function calculateBinarySimilarity(str1, str2) {
+    if (str1.length !== str2.length) {
+        throw new Error("Strings must have the same length for comparison.");
+    }
+
+    const totalBytes = str1.length;
+    let matchingBytes = 0;
+
+    for (let i = 0; i < totalBytes; i += 2) {
+        const byte1 = str1.slice(i, i + 2);
+        const byte2 = str2.slice(i, i + 2);
+
+        if (byte1 === byte2) {
+            matchingBytes += 2; // Assuming each character represents a byte in hexadecimal
+        }
+    }
+
+    const similarityPercentage = (matchingBytes / totalBytes) * 100;
+
+    return similarityPercentage.toFixed(2);
+}
 
 module.exports = router;
